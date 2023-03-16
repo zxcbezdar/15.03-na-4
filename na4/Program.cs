b960f0c0b8d5037b17f57a431a21ed075644e267
zxcbezdar <maksikplayb@gmail.com>
2023-03-16 22:27:10 +0300

    commit

---
+++ na4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("первое число = ");
+        int start = int.Parse(Console.ReadLine()); 
+        Console.Write("конечное число = ");
+        int end = int.Parse(Console.ReadLine()); 
+
+        for (int i = start; i <= end; i++)
+        {
+            bool isPrime = true;
+            for (int j = 2; j <= Math.Sqrt(i); j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+
